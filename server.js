780d9d7f61a92dae93db47a07ebb88c2de504dd3
Nguyen Gia Nghi <146867533+Nghi-creator@users.noreply.github.com>
2025-10-09 23:19:43 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
